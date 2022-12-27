e87dd44e441653d3aa93d44dbcf2f0234ca5c95d
juanitoo28 <60743021+juanitoo28@users.noreply.github.com>
2022-12-27 16:23:52 +0100

    Ajout de l'application 'repas'

--- src/Ratatouille/views.py
+++ src/Ratatouille/views.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context={"date": datetime.today()})
