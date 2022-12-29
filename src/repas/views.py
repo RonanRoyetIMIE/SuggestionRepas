115a6cffbb6d8da3c9ced150bc1f8f46173b6bb4
juanitoo28 <60743021+juanitoo28@users.noreply.github.com>
2022-12-29 14:52:48 +0100

    Récupération des données de l'API part1

--- src/repas/views.py
+++ src/repas/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+import requests
+
+url = "https://world.openfoodfacts.org/?json=true"
+
+reponse = requests.get(url)
+print("Récupération des données de l'API:")
+print(reponse)
+contenu = reponse.json()
+print(contenu)
 
 def index(request):
     return render(request, "repas/index.html")
