e6f3a954a557667ce61b3125d8b09899922712fa
Ronan Royet <royet_r@etna-alternance.net>
2022-12-29 14:05:29 +0100

    Add CORS handling, remove first_name last_name from user

--- SuggestionRepas/settings.py
+++ SuggestionRepas/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'SuggestionRepas.urls'
@@ -141,3 +145,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+    'http://127.0.0.1:4200',
+]
--- api/serializers.py
+++ api/serializers.py
@@ -20,11 +20,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
+        fields = ('username', 'password', 'password2', 'email')
+        
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -36,9 +33,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         try:
             user = User.objects.create(
                 username=validated_data['username'],
-                email=validated_data['email'],
-                first_name=validated_data['first_name'],
-                last_name=validated_data['last_name']
+                email=validated_data['email']
             )
             user.set_password(validated_data['password'])
             try:
